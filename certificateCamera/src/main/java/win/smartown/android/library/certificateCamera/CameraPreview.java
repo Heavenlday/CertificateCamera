4787d1da504fd795ba23fab0eda0aa04c3494b9b
HuangPengHua <Heavenlday@users.noreply.github.com>
2018-05-31 11:07:12 +0800

    Update CameraPreview.java
    修复拍照卡住问题

--- certificateCamera/src/main/java/win/smartown/android/library/certificateCamera/CameraPreview.java
+++ certificateCamera/src/main/java/win/smartown/android/library/certificateCamera/CameraPreview.java
@@ -181,7 +181,7 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     public void takePhoto(Camera.PictureCallback pictureCallback) {
         if (camera != null) {
             camera.takePicture(null, null, pictureCallback);
-            camera.stopPreview();
+         //   camera.stopPreview();
         }
     }
 
